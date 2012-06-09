d62d7617c07b4bfab3dc9463879102b9ad292237
Ralf Kistner <ralf.kistner@gmail.com>
2012-06-09 23:13:01 +0200

    Updated template.

--- templates/gcj.py
+++ templates/gcj.py
@@ -1,9 +1,13 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+# Uses https://github.com/rkistner/contest-algorithms
+
 import sys
 fin = sys.stdin
 T = int(fin.readline())
 for case in range(1,T+1):
-    N, M = map(int, fin.readline().split())
-    
+    N = int(fin.readline())
+    numbers = map(int, fin.readline().split())
+
     print("Case #%d: %s" % (case, "result"))
 
